0798ef34fc6e5d985f7a322e28a1d127486adb46
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-09-19 16:43:14 +0900

    feat: integrate middleware session

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-console.log("2-qadam")
+console.log("2-qadam");
 
 import express from "express";
 import path from "path";
@@ -7,6 +7,14 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 // import router from "./router";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "session",
+});
 
 // kirish
 const app = express();
@@ -16,8 +24,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
-//view
+// sessions
 
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, //3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//view
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
