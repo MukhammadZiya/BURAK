c69e6011007b310da5c971d48ad694b9f5367947
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-10-18 23:21:22 +0900

    feat: develop  getRestaurant rest API

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -18,6 +18,17 @@ class MemberService {
   }
 
   /** SPA */
+  public async getRestaurant(): Promise<Member> {
+    const result = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURANT })
+      .lean()
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Messege.NO_DATA_FOUND);
+
+    return result;
+  }
+
   public async signup(input: MemberInput): Promise<Member> {
     const salt = await bcrypt.genSalt();
     input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
@@ -92,9 +103,8 @@ class MemberService {
       .limit(4)
       .exec();
 
-
-      if(!result) throw new Errors(HttpCode.NOT_FOUND, Messege.NO_DATA_FOUND)
-    return  result;
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Messege.NO_DATA_FOUND);
+    return result;
   }
 
   /** SSR */
